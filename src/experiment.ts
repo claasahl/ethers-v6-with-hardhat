5e8e4593dfb0a4fc62910717264842cbbc121dd6
Claas Ahlrichs <claasahl@gmail.com>
2023-07-19 09:13:49 +0000

    revised experiment

--- src/experiment.ts
+++ src/experiment.ts
@@ -1,20 +1,37 @@
-import { ContractFactory, JsonRpcProvider, Wallet } from "ethers";
+import { AddressLike, BaseContract, ContractFactory, JsonRpcProvider, Overrides, Wallet } from "ethers";
 import { abi as ERC20ABI, bytecode as ERC20ByteCode } from "@uniswap/v2-periphery/build/ERC20.json";
 
+type TokenContract = BaseContract & {
+    name(overides?: Overrides): Promise<string>;
+    symbol(overides?: Overrides): Promise<string>;
+    decimals(overides?: Overrides): Promise<number>;
+    totalSupply(overides?: Overrides): Promise<bigint>;
+    balanceOf(owner: AddressLike, overides?: Overrides): Promise<bigint>;
+    transfer(to: AddressLike, value: bigint, overides?: Overrides): Promise<boolean>;
+    transferFrom(from: AddressLike, to: AddressLike, value: bigint, overides?: Overrides): Promise<boolean>;
+    approve(spender: AddressLike, value: bigint, overides?: Overrides): Promise<boolean>;
+    allowance(owner: AddressLike, spender: AddressLike, overides?: Overrides): Promise<bigint>;
+}
 
 async function main() {
     console.log("connecting ...");
     const initialSupply = 100000000000n;
     const provider = new JsonRpcProvider("http://localhost:8545");
+    provider.pollingInterval = 250;
     await provider.ready
     console.log("connected");
 
     const signer = new Wallet("0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e", provider);
-    const factory = new ContractFactory(ERC20ABI, ERC20ByteCode, signer);
+    const factory = new ContractFactory<any[], TokenContract>(ERC20ABI, ERC20ByteCode, signer);
+    console.log("deploying ERC20 token");
+    const contract = await factory.deploy(initialSupply);
+    await contract.waitForDeployment();
+    console.log("ERC20 token deployed to %s", await contract.getAddress());
     for(let index = 0; index <= 5; index++) {
-        console.log("deploying ERC20 token #%d", index);
-        const contract = await factory.deploy(initialSupply);
-        console.log("ERC20 token #%d deployed to %d", index, await contract.getAddress());
+        console.log("approving 'someone' to spend ERC20 token");
+        const fractionOfSupply = initialSupply / 1000n;
+        const someone = "0x0000000000000000000000000000000000000000";
+        await contract.approve(someone, fractionOfSupply)
     }
     console.log("done")
 }
